fa37d336f1df1747ddba630467c7c15b146c0881
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-15 11:59:14 -0300

    creation of new routes for orders data

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 const productRoute = require('./routes/produtos');
+const orderRoute = require('./routes/pedidos');
+
 app.use('/produtos',productRoute);
+app.use('/pedidos',orderRoute);
+
 
 app.use('/teste',(req, res, next) => {
     res.status(200).send({
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -2,40 +2,34 @@ const express = require('express');
 const router = express.Router();
 router.get('/', (req, res, next) => {
     res.status(200).send({
-        msg: "usando GET na rota de produtos"
+        msg: "usando GET na rota de pedidos"
     })
 });
 
 router.post('/', (req, res, next) => {
     res.status(201).send({
-        msg: "usando POST na rota de produtos"
+        msg: "usando POST na rota de pedido"
     })
 });
 
-router.get('/:id_produto', (req, res, next) => {
-    let num = req.params.id_produto;
+router.get('/:id_ped', (req, res, next) => {
+    let num = req.params.idped;
     if (num === "especial") {
         res.status(200).send({
-            msg: "usando o get de apenas um produto super especial",
+            msg: "usando o get de apenas um pedidos super especial",
             id: num
         })
     } else {
         res.status(200).send({
-            msg: "usando o get de apenas um produto",
+            msg: "usando o get de apenas um pedidos",
             id: num
         })
     }
 });
 
-router.patch('/:id', (req, res, next) => {
-    res.status(200).send({
-        msg: "usando PATCH na rota de produtos"
-    })
-});
-
 router.delete('/:id', (req, res, next) => {
     res.status(200).send({
-        msg: "usando DELETE na rota de produtos"
+        msg: "usando DELETE na rota de pedidos"
     })
 });
 
