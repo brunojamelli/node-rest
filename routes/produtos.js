af81282e88d9095f620ec63daf8fdb26b14bf0a4
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-15 20:28:38 -0300

    products mysql database actions in node

--- routes/produtos.js
+++ routes/produtos.js
@@ -3,9 +3,28 @@ const router = express.Router();
 const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        msg: "usando GET na rota de produtos"
-    })
+
+    // res.status(200).send({
+    //     msg: "usando GET na rota de produtos"
+    // });
+
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'SELECT * FROM products',
+            (error, result, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(200).send({
+                    response: result
+                })
+            }
+        )
+    });
 });
 
 router.post('/', (req, res, next) => {
@@ -13,12 +32,12 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     }
-    mysql.getConnection((error, con) => {
-        con.query('INSERT INTO products (name, price, id_product) VALUES(?,?);'),
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO products (name, price) VALUES(?,?)',
             [req.body.name, req.body.price],
-
             (error, result, field) => {
-                con.release();
+                conn.release();
                 if (error) {
                     return res.status(500).send({
                         error: error,
@@ -29,31 +48,59 @@ router.post('/', (req, res, next) => {
                     msg: "product created",
                     id_product: result.insertId
                 })
-
             }
+        )
     });
+
 
 });
 
 router.get('/:id_produto', (req, res, next) => {
-    let num = req.params.id_produto;
-    if (num === "especial") {
-        res.status(200).send({
-            msg: "usando o get de apenas um produto super especial meesmo",
-            id: num
-        })
-    } else {
-        res.status(200).send({
-            msg: "usando o get de apenas um produto",
-            id: num
-        })
-    }
+   
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'SELECT * FROM products where productId = ?;',
+            [req.params.id_produto],
+            (error, result, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(200).send({
+                    response: result
+                })
+            }
+        )
+    });
 });
 
 router.patch('/:id', (req, res, next) => {
-    res.status(200).send({
-        msg: "usando PATCH na rota de produtos"
-    })
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    mysql.getConnection((error, conn) => {
+        conn.query(
+            'INSERT INTO products (name, price) VALUES(?,?)',
+            [req.body.name, req.body.price],
+            (error, result, field) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({
+                        error: error,
+                        response: null
+                    });
+                }
+                res.status(201).send({
+                    msg: "product created",
+                    id_product: result.insertId
+                })
+            }
+        )
+    });
 });
 
 router.delete('/:id', (req, res, next) => {
